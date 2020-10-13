00d97a7b2560dc4b4a4748fb0de1625d6d791415
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2020-10-13 23:23:27 +0400

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = Object.fromEntries(bracketsConfig);
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (stack[stack.length-1] === str[i]){
+      stack.pop();
+    }
+    else if (brackets[str[i]]) {
+      stack.push(brackets[str[i]]);
+    }
+    else {
+      return false;
+    }
+  }
+  return stack.length === 0;
 }
